b355eb3f6ce2851d23836fa77ee6d20514be1a73
Kuei <kuei@KRs-MBA.local>
2022-11-28 13:43:49 +0800

    L21-273 TO DO LIST V1

---
+++ app.js
@@ -0,0 +1,54 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["1", "2", "3"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+
+    res.render("list", {
+      listTitle: day, newListItems: items
+    });
+
+});
+
+app.post("/", function(req, res) {
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  };
+
+});
+
+app.get("/work", function(req,res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+// 為什麼lisent中的function不需要req, res?
+app.listen("3000", function() {
+  console.log("Server is running on PORT 3000.");
+});
